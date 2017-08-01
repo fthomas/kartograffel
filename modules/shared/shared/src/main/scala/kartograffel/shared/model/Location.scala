db7bf642ea9c9732f228714f247bf29cd1458006
Frank S. Thomas <frank@timepit.eu>
2017-08-01 22:47:35 +0200

    Remove package object

--- modules/shared/shared/src/main/scala/kartograffel/shared/model/Location.scala
+++ modules/shared/shared/src/main/scala/kartograffel/shared/model/Location.scala
@@ -1,8 +1,12 @@
 package kartograffel.shared.model
 
+import eu.timepit.refined.W
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Interval
 import io.circe.generic.semiauto._
 import io.circe.{Decoder, Encoder}
 import io.circe.refined._
+import kartograffel.shared.model.Location.{Latitude, Longitude}
 
 final case class Location(
     latitude: Latitude,
@@ -10,6 +14,10 @@ final case class Location(
 )
 
 object Location {
+  type Latitude = Double Refined Interval.Closed[W.`-90.0`.T, W.`90.0`.T]
+
+  type Longitude = Double Refined Interval.Closed[W.`-180.0`.T, W.`180.0`.T]
+
   implicit val locationDecoder: Decoder[Location] =
     deriveDecoder
 
