49ab9e9cee5c415f7c9957a484bfbf98d4cb508c
Frank S. Thomas <frank@timepit.eu>
2017-08-02 18:49:54 +0200

    Add function Coordinates => Location

--- modules/client/js/src/main/scala/kartograffel/Client.scala
+++ modules/client/js/src/main/scala/kartograffel/Client.scala
@@ -3,19 +3,21 @@ package kartograffel
 import eu.timepit.refined.api.RefType
 import kartograffel.shared.model.Location
 import kartograffel.shared.model.Location.{Latitude, Longitude}
+import org.scalajs.dom.{window, Coordinates}
 
 object Client {
   def main(args: Array[String]): Unit = {
-    org.scalajs.dom.window.navigator.geolocation.getCurrentPosition { pos =>
-      val loc = for {
-        lat <- RefType.applyRef[Latitude](pos.coords.latitude)
-        lon <- RefType.applyRef[Longitude](pos.coords.longitude)
-      } yield Location(lat, lon)
-
-      println("lat:" + pos.coords.latitude)
-      println("lon: " + pos.coords.longitude)
-      println(loc)
+    window.navigator.geolocation.getCurrentPosition { pos =>
+      println(locationFrom(pos.coords))
     }
     println("Hello, world!")
+  }
+
+  def locationFrom(coordinates: Coordinates): Option[Location] = {
+    val location = for {
+      latitude <- RefType.applyRef[Latitude](coordinates.latitude)
+      longitude <- RefType.applyRef[Longitude](coordinates.longitude)
+    } yield Location(latitude, longitude)
+    location.toOption
   }
 }
