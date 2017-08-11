b5e9c07c08957ce6a9189c2195c0ab8599eb0cb1
Frank S. Thomas <frank@timepit.eu>
2017-08-11 06:46:06 +0200

    Tweak blazeBuilder

--- modules/server/jvm/src/main/scala/kartograffel/server/Server.scala
+++ modules/server/jvm/src/main/scala/kartograffel/server/Server.scala
@@ -1,26 +1,25 @@
 package kartograffel.server
 
-import doobie.util.transactor.Transactor
 import eu.timepit.refined.auto._
+import fs2.interop.cats._
 import fs2.{Stream, Task}
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.util.StreamApp
-import fs2.interop.cats._
 
 object Server extends StreamApp {
   override def stream(args: List[String]): Stream[Task, Nothing] =
     Stream.eval(Config.load).flatMap { config =>
       val transactor = Storage.transactorFrom(config.db)
+      val gr = GraffelRepository.withTransactor(transactor)
       Storage.create2(transactor)
-      blazeBuilder(config, transactor).serve
+      blazeBuilder(config.http, gr).serve
     }
 
-  def blazeBuilder(config: Config,
-                   transactor: Transactor[Task]): BlazeBuilder =
+  def blazeBuilder(httpConfig: Config.Http,
+                   gr: GraffelRepository[Task]): BlazeBuilder =
     BlazeBuilder
-      .bindHttp(config.http.port, config.http.host)
+      .bindHttp(httpConfig.port, httpConfig.host)
       .mountService(Service.root)
-      .mountService(Service.api(GraffelRepository.withTransactor(transactor)),
-                    "/api")
+      .mountService(Service.api(gr), "/api")
       .mountService(Service.assets, s"/${BuildInfo.assetsRoot}")
 }
--- modules/server/jvm/src/test/scala/kartograffel/server/ServerSpec.scala
+++ modules/server/jvm/src/test/scala/kartograffel/server/ServerSpec.scala
@@ -1,13 +1,13 @@
 package kartograffel.server
 
 import eu.timepit.refined.auto._
-import kartograffel.server.Config.Http
 import org.specs2.mutable.Specification
 
 class ServerSpec extends Specification {
   "Server" >> {
     "call blazeBuilder" >> {
-      Server.blazeBuilder(Config(Http("::", 8080), null), null)
+      val httpConfig = Config.Http("::", 8080)
+      Server.blazeBuilder(httpConfig, null)
       true
     }
     "call stream" >> {
