cc1beb43955ccfe4fc3096cf60ce87cd64bd8e54
Frank S. Thomas <frank@timepit.eu>
2017-08-15 00:15:33 +0200

    Rewrite doobie Fragments

--- modules/server/jvm/src/main/scala/kartograffel/server/db/migration/V0001__CreateGraffel.scala
+++ modules/server/jvm/src/main/scala/kartograffel/server/db/migration/V0001__CreateGraffel.scala
@@ -4,14 +4,12 @@ import doobie.imports._
 import kartograffel.server.db.DoobieMigration
 
 class V0001__CreateGraffel extends DoobieMigration {
-  override def migrate: ConnectionIO[_] = {
-    val create: Update0 = sql"""
+  override def migrate: ConnectionIO[_] =
+    (sql"""
       CREATE TABLE graffel (
         id BIGSERIAL PRIMARY KEY,
         latitude  DOUBLE NOT NULL,
         longitude DOUBLE NOT NULL
       )
-    """.update
-    create.run
-  }
+    """: Fragment).update.run
 }
