a13ba605f12fd03efcd1fdbe5f0a84fe53817dc1
Frank S. Thomas <frank@timepit.eu>
2018-01-31 21:23:57 +0100

    Test RefinedQueryParamDecoder

---
+++ modules/server/jvm/src/test/scala/kartograffel/server/infrastructure/http4s/RefinedQueryParamDecoderTest.scala
@@ -0,0 +1,22 @@
+package kartograffel.server.infrastructure.http4s
+
+import cats.data.Validated
+import eu.timepit.refined.types.numeric.NonNegLong
+import kartograffel.server.infrastructure.http4s.refined._
+import org.http4s.{ParseFailure, QueryParamDecoder, QueryParameterValue}
+import org.specs2.matcher.Matchers
+import org.specs2.mutable.Specification
+
+class RefinedQueryParamDecoderTest extends Specification with Matchers {
+  "QueryParamDecoder[NonNegLong](1)" >> {
+    QueryParamDecoder[NonNegLong].decode(QueryParameterValue("1")) must_===
+      Validated.validNel(NonNegLong(1))
+  }
+
+  "QueryParamDecoder[NonNegLong](-1)" >> {
+    QueryParamDecoder[NonNegLong].decode(QueryParameterValue("-1")) must_===
+      Validated.invalidNel(
+        ParseFailure("Predicate (-1 < 0) did not fail.",
+                     "Predicate (-1 < 0) did not fail."))
+  }
+}
