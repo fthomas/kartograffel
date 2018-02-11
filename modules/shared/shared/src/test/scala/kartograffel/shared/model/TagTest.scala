37c7f6f00bd3112a1ea00d8b3592d85a4f0df24a
Frank S. Thomas <frank@timepit.eu>
2018-02-11 14:07:44 +0100

    Test Tag de and encoder

---
+++ modules/shared/shared/src/test/scala/kartograffel/shared/model/TagTest.scala
@@ -0,0 +1,11 @@
+package kartograffel.shared.model
+
+import io.circe.testing.CodecTests
+import io.circe.testing.instances._
+import kartograffel.shared.model.ArbitraryInstances._
+import org.scalatest.FunSuite
+import org.typelevel.discipline.scalatest.Discipline
+
+class TagTest extends FunSuite with Discipline {
+  checkAll("Tag", CodecTests[Tag].codec)
+}
