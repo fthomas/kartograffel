2eaaa630df6449e26455242afb3f821e9726bf27
Frank S. Thomas <frank@timepit.eu>
2018-02-15 17:35:28 +0100

    Reminder to test DbFileRepository

---
+++ modules/server/jvm/src/test/scala/kartograffel/server/infrastructure/doobie/repository/DbFileRepositoryTest.scala
@@ -0,0 +1,5 @@
+package kartograffel.server.infrastructure.doobie.repository
+
+//import kartograffel.shared.model.ArbitraryInstances._
+
+//class DbFileRepositoryTest extends DbEntityRepositoryTest(DbFileRepository)
