a2b710c913bc43841633fc5014ae37b12a47c46c
Frank S. Thomas <frank@timepit.eu>
2018-05-23 22:14:40 +0200

    Use RefinedTypeOps.Numeric for Longitude and Latitude
    This allows to use Longitude.{Min,Max}Value, etc.

--- modules/shared/shared/src/main/scala/kartograffel/shared/domain/model/package.scala
+++ modules/shared/shared/src/main/scala/kartograffel/shared/domain/model/package.scala
@@ -6,10 +6,8 @@ import eu.timepit.refined.numeric.Interval
 
 package object model {
   type Latitude = Double Refined Interval.Closed[W.`-90.0`.T, W.`90.0`.T]
-
-  object Latitude extends RefinedTypeOps[Latitude, Double]
+  object Latitude extends RefinedTypeOps.Numeric[Latitude, Double]
 
   type Longitude = Double Refined Interval.Closed[W.`-180.0`.T, W.`180.0`.T]
-
-  object Longitude extends RefinedTypeOps[Longitude, Double]
+  object Longitude extends RefinedTypeOps.Numeric[Longitude, Double]
 }
