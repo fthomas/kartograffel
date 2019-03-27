ac466df4c24be5d980304f5006a7ca3a3a52e3eb
kalejami <rawshell@gmx.net>
2019-03-27 20:09:14 +0100

    AsyncCallback GeoLocation

--- modules/client/js/src/main/scala/kartograffel/client/component/TagComponent.scala
+++ modules/client/js/src/main/scala/kartograffel/client/component/TagComponent.scala
@@ -3,18 +3,35 @@ package kartograffel.client.component
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.component.Scala.Unmounted
 import japgolly.scalajs.react.vdom.html_<^._
-import kartograffel.shared.domain.model._
+import kartograffel.client.geolocation.GeoLocation
+import kartograffel.shared.domain.model.Position
 import kartograffel.shared.ui.model.TagView
+
+import scala.util.{Failure, Success}
 
 object TagComponent {
 
-  case class State(tags: List[TagView] = List.empty, tagInput: String = "")
+  final case class State(tags: List[TagView] = List.empty,
+                         tagInput: String = "",
+                         position: Option[Position] = Option.empty)
 
-  class Backend(scope: BackendScope[Unit, State]) {
+  final case class Backend($ : BackendScope[Unit, State]) {
+
+    def init: Callback =
+      GeoLocation.position.completeWith(pos =>
+        pos match {
+          case Success(p)         => $.modState(_.copy(position = Some(p)))
+          case Failure(exception) => Callback(exception.printStackTrace())
+      })
+
     def render(state: State): VdomElement =
       <.div(
-        LeafletComponent(
-          LeafletComponent.Props(400, 400, List.empty, Position(Latitude(48d), Longitude(12d)))))
+        state.position.whenDefined(
+          p =>
+            LeafletComponent(
+              LeafletComponent.Props(600, 600, List.empty, p)
+          ))
+      )
 
   }
 
@@ -22,6 +39,7 @@ object TagComponent {
     .builder[Unit]("TagComponent")
     .initialState(State())
     .renderBackend[Backend]
+    .componentDidMount(_.backend.init)
     .build
 
   def apply(): Unmounted[Unit, State, Backend] = component()
