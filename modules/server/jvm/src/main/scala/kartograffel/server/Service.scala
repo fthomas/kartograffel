bd1d6fd802f3afbb1abe62b9f5cb208a021e0873
kalejami <rawshell@gmx.net>
2019-03-29 19:46:21 +0100

    New Rest-Methods for Service

--- modules/server/jvm/src/main/scala/kartograffel/server/Service.scala
+++ modules/server/jvm/src/main/scala/kartograffel/server/Service.scala
@@ -3,18 +3,20 @@ package kartograffel.server
 import java.util.UUID
 
 import cats.effect.IO
+import doobie.implicits._
 import doobie.util.transactor.Transactor
 import io.circe.syntax._
+import kartograffel.server.domain.model.Radius.{Length, LengthRange}
 import kartograffel.server.domain.model._
-import kartograffel.server.infrastructure.doobie.repository.DbGraffelRepository
+import kartograffel.server.infrastructure.doobie.service.DbGraffelService
 import kartograffel.server.infrastructure.http4s.refined._
 import kartograffel.shared.domain.model._
 import org.http4s.circe._
 import org.http4s.dsl.io._
 import org.http4s.headers._
 import org.http4s.{HttpRoutes, MediaType}
-
-import doobie.implicits._
+import eu.timepit.refined._
+import kartograffel.shared.ui.model.TagView
 
 object Service {
 
@@ -38,9 +40,25 @@ object Service {
   def api(tx: Transactor[IO]): HttpRoutes[IO] =
     HttpRoutes.of[IO] {
       case GET -> Root / "graffel" / UUIDVar(id) =>
-        DbGraffelRepository.findById(GraffelId(id)).transact(tx).flatMap {
+        DbGraffelService.findById(GraffelId(id)).transact(tx).flatMap {
           case Some(entity) => Ok(entity.asJson)
           case None         => NotFound()
+        }
+
+      case GET -> Root / "graffels" :? LatQueryParamMatcher(lat) +& LonQueryParamMatcher(lon) =>
+        val length: Length = refineMV[LengthRange](1)
+        DbGraffelService
+          .findNearPosition(Position(lat, lon), Radius(length, kilometer))
+          .transact(tx)
+          .flatMap(l => Ok(l.map(t => TagView(t._1.name, t._2.position)).asJson))
+
+      case request @ PUT -> Root / "graffel" =>
+        request.decodeJson[(String, Position)].flatMap {
+          case (name, position) =>
+            DbGraffelService
+              .create(name, position)
+              .transact(tx)
+              .flatMap(_ => Ok())
         }
 
       case GET -> Root / "version" => Ok(BuildInfo.version.asJson)
