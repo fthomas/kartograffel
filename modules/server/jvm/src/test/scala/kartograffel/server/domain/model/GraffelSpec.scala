086b491f934a084cda50ed13dd6e805db2f2bde4
Scala Steward <me@scala-steward.org>
2020-08-22 10:47:59 +0200

    Update scalatest to 3.2.2

--- modules/server/jvm/src/test/scala/kartograffel/server/domain/model/GraffelSpec.scala
+++ modules/server/jvm/src/test/scala/kartograffel/server/domain/model/GraffelSpec.scala
@@ -2,11 +2,11 @@ package kartograffel.server.domain.model
 
 import io.circe.testing.CodecTests
 import io.circe.testing.instances._
-import org.scalatest.FunSuite
 import org.typelevel.discipline.scalatest.Discipline
 
 import kartograffel.server.ArbitraryInstances._
+import org.scalatest.funsuite.AnyFunSuite
 
-class GraffelSpec extends FunSuite with Discipline {
+class GraffelSpec extends AnyFunSuite with Discipline {
   checkAll("Graffel", CodecTests[Graffel].codec)
 }
